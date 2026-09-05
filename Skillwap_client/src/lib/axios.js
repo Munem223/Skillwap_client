3e0e9c6b7a2b664afd63e298ac79f3d8927ce567
Munem223 <munemshahriar223@gmail.com>
2026-09-05 22:50:40 +0600

    axios

--- Skillwap_client/src/lib/axios.js
+++ Skillwap_client/src/lib/axios.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
-
 const api = axios.create({
 
-    baseURL:"http://localhost:5000/api",
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
 
     withCredentials:true
 
 });
-
 
 export default api;
